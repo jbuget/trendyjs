ad12d74de40c7c373ddc2b0d35bc59d0c6944ab6
Jérémy Buget <jbuget@gmail.com>
2017-04-03 09:14:51 +0200

    Add first test using Mongoose Model

--- test/services/snapshots_test.js
+++ test/services/snapshots_test.js
@@ -1,32 +1,85 @@
-import { describe, it } from 'mocha';
-import { expect } from 'chai';
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { expect, use } from 'chai';
+import sinon from 'sinon';
+import mongoose from 'mongoose';
+import sinonChai from 'sinon-chai';
+import sinonMongoose from 'sinon-mongoose';
+import dates from '../../lib/infrastructure/dates';
+import Snapshot from '../../lib/domain/snapshot';
+import Service  from '../../lib/domain/service';
+import snapshots from '../../lib/domain/snapshots';
+require('chai').use(sinonChai);
 
-/*
- function _findSnapshotOfToday() {
- return new Promise((resolve, reject) => {
- const today = new Date();
- today.setHours(0, 0, 0, 0);
- const tomorrow = new Date();
- tomorrow.setDate(today.getDate() + 1);
- tomorrow.setHours(0, 0, 0, 0);
+describe.only('Services | snapshots', function () {
 
- Snapshot.findOne({
- created_at: {
- $gte: today,
- $lt: tomorrow
- }
- }).exec()
- .then(resolve)
- .catch(reject);
- });
- }
- */
+  describe('#fetchAndFeedDataFromReferential', function () {
 
-describe('Services | snapshots', function () {
+    let ServiceMock;
 
-  describe('#findSnapshotOfToday()', function () {
+    beforeEach(function() {
+        ServiceMock = sinon.stub(Service, 'find');
+    });
 
-    // TODO
+    afterEach(function() {
+        ServiceMock.restore();
+    });
+
+    it('should return a promise resolving with an array of services object initiated from referential data', function (done) {
+      // given
+      const refObjects = [
+        new Service({
+          ref: 'trendyjs_serviceA',
+          npmPackage: 'npmPackage_serviceA',
+          githubRepository: 'githubRepository_serviceA',
+          stackoverflowTag: 'stackoverflowTag_serviceA'
+        }),
+        new Service({
+          ref: 'trendyjs_serviceB',
+          npmPackage: 'npmPackage_serviceB',
+          githubRepository: 'githubRepository_serviceB',
+          stackoverflowTag: 'stackoverflowTag_serviceB'
+        }),
+        new Service({
+          ref: 'trendyjs_serviceC',
+          npmPackage: 'npmPackage_serviceC',
+          githubRepository: 'githubRepository_serviceC',
+          stackoverflowTag: 'stackoverflowTag_serviceC'
+        })
+      ];
+      ServiceMock.returns({
+        exec: () => Promise.resolve(refObjects)
+      });
+
+      // when
+      snapshots.fetchAndFeedDataFromReferential().then((services) => {
+
+        // then
+        const expectedServices = [{
+          references: {
+            trendyjs: 'trendyjs_serviceA',
+            npmjs: 'npmPackage_serviceA',
+            github: 'githubRepository_serviceA',
+            stackoverflow: 'stackoverflowTag_serviceA'
+          }
+        }, {
+          references: {
+            trendyjs: 'trendyjs_serviceB',
+            npmjs: 'npmPackage_serviceB',
+            github: 'githubRepository_serviceB',
+            stackoverflow: 'stackoverflowTag_serviceB'
+          }
+        }, {
+          references: {
+            trendyjs: 'trendyjs_serviceC',
+            npmjs: 'npmPackage_serviceC',
+            github: 'githubRepository_serviceC',
+            stackoverflow: 'stackoverflowTag_serviceC'
+          }
+        }];
+        expect(services).to.deep.equal(expectedServices);
+        done();
+      });
+    });
 
   });
 });
