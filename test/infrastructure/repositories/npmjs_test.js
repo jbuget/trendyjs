04eb85d36b57391e84c7155a316a3ce9f5a79f72
Jérémy Buget <jbuget@gmail.com>
2017-05-08 17:57:09 +0200

    Fix NOPMJS API call

--- test/infrastructure/repositories/npmjs_test.js
+++ test/infrastructure/repositories/npmjs_test.js
@@ -19,7 +19,7 @@ describe('NPMjs', function () {
 
     beforeEach(function () {
       getStub = stub(request, 'get').callsFake((uri, cb) => {
-        cb(null, null, {})
+        cb(null, null, JSON.stringify({}));
       });
     });
 
@@ -57,73 +57,74 @@ describe('NPMjs', function () {
 
     it('should return NPMJS registry last day, week & month data for each given service', () => {
       // given
+      const lastDayData = JSON.stringify({
+        "dataSource_A": {
+          "downloads": 10,
+          "package": "dataSource_A",
+          "start": "2017-05-07",
+          "end": "2017-05-07"
+        },
+        "dataSource_B": {
+          "downloads": 20,
+          "package": "dataSource_B",
+          "start": "2017-05-07",
+          "end": "2017-05-07"
+        },
+        "dataSource_C": {
+          "downloads": 30,
+          "package": "dataSource_C",
+          "start": "2017-05-07",
+          "end": "2017-05-07"
+        }
+      });
+      const lastWeekData = JSON.stringify({
+        "dataSource_A": {
+          "downloads": 100,
+          "package": "dataSource_A",
+          "start": "2017-05-01",
+          "end": "2017-05-07"
+        },
+        "dataSource_B": {
+          "downloads": 200,
+          "package": "dataSource_B",
+          "start": "2017-05-01",
+          "end": "2017-05-07"
+        },
+        "dataSource_C": {
+          "downloads": 300,
+          "package": "dataSource_C",
+          "start": "2017-05-01",
+          "end": "2017-05-07"
+        }
+      });
+      const lastMonthData = JSON.stringify({
+        "dataSource_A": {
+          "downloads": 1000,
+          "package": "dataSource_A",
+          "start": "2017-04-08",
+          "end": "2017-05-07"
+        },
+        "dataSource_B": {
+          "downloads": 2000,
+          "package": "dataSource_B",
+          "start": "2017-04-08",
+          "end": "2017-05-07"
+        },
+        "dataSource_C": {
+          "downloads": 3000,
+          "package": "dataSource_C",
+          "start": "2017-04-08",
+          "end": "2017-05-07"
+        }
+      });
       getStub.onCall(0).callsFake((uri, cb) => {
-        cb(null, null, {
-          "dataSource_A": {
-            "downloads": 10,
-            "package": "dataSource_A",
-            "start": "2017-05-07",
-            "end": "2017-05-07"
-          },
-          "dataSource_B": {
-            "downloads": 20,
-            "package": "dataSource_B",
-            "start": "2017-05-07",
-            "end": "2017-05-07"
-          },
-          "dataSource_C": {
-            "downloads": 30,
-            "package": "dataSource_C",
-            "start": "2017-05-07",
-            "end": "2017-05-07"
-          }
-        });
+        cb(null, null, lastDayData);
       });
-
       getStub.onCall(1).callsFake((uri, cb) => {
-        cb(null, null, {
-          "dataSource_A": {
-            "downloads": 100,
-            "package": "dataSource_A",
-            "start": "2017-05-01",
-            "end": "2017-05-07"
-          },
-          "dataSource_B": {
-            "downloads": 200,
-            "package": "dataSource_B",
-            "start": "2017-05-01",
-            "end": "2017-05-07"
-          },
-          "dataSource_C": {
-            "downloads": 300,
-            "package": "dataSource_C",
-            "start": "2017-05-01",
-            "end": "2017-05-07"
-          }
-        });
+        cb(null, null, lastWeekData);
       });
-
       getStub.onCall(2).callsFake((uri, cb) => {
-        cb(null, null, {
-          "dataSource_A": {
-            "downloads": 1000,
-            "package": "dataSource_A",
-            "start": "2017-04-08",
-            "end": "2017-05-07"
-          },
-          "dataSource_B": {
-            "downloads": 2000,
-            "package": "dataSource_B",
-            "start": "2017-04-08",
-            "end": "2017-05-07"
-          },
-          "dataSource_C": {
-            "downloads": 3000,
-            "package": "dataSource_C",
-            "start": "2017-04-08",
-            "end": "2017-05-07"
-          }
-        });
+        cb(null, null, lastMonthData);
       });
 
       // when
